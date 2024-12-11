9ab34dab4d3148093f78e1b023ce70f8e960ab57
kimyechan <tioon74@naver.com>
2024-12-12 00:57:26 +0900

    commit : 알고리즘 1주차 업데이트

--- src/main/java/algorithmStudy/주차1/BeakJoon_3109.java
+++ src/main/java/algorithmStudy/주차1/BeakJoon_3109.java
@@ -5,18 +5,21 @@ import java.util.Arrays;
 
 // 빵집 (1주차 1번문제)
 public class BeakJoon_3109 {
-    static int[] x = {-1,-1,-1};
-    static int[] y = {-1,0,1};
+    static int r;
+    static int c;
+    static boolean[][] check;
+    static char[][] array;
+    static int answer=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] s1 = br.readLine().split(" ");
-        int r = Integer.parseInt(s1[0]);
-        int c = Integer.parseInt(s1[1]);
+        r = Integer.parseInt(s1[0]);
+        c = Integer.parseInt(s1[1]);
 
-        char[][] array = new char[r][c];
-        int[][] value = new int[r][c];
+        array = new char[r][c];
+        check = new boolean[r][c];
 
         for(int i=0; i<r; i++){
             String[] s2 = br.readLine().split("");
@@ -25,16 +28,42 @@ public class BeakJoon_3109 {
             }
         }
 
+        // 빵집 가스관 시작
         for(int i=0; i<r; i++){
-            if(array[0][i] == '.'){
-                value[0][i] = 1;
+            if(dfs(i,0)){ // true면 도착점까지 도착했으므로 카운트 증가
+                answer++;
             }
+
         }
 
-        for(int i=1; i<r; i++){
-            for(int j=c-1; j>=0; j--){
 
+
+        System.out.println(answer);
+    }
+
+    public static boolean dfs(int y, int x){
+        int[] next = {-1,0,1};
+
+        check[y][x] = true; // 도착 표시
+
+        // 원웅이 빵집 도착 시, true
+        if(x == c-1){
+            return true;
+        }
+
+        for(int i=0; i<3; i++){
+            int nextY = y+next[i];
+            int nextX = x+1;
+
+            if(nextY>=0 && nextY<r){
+                // 건물이 없고, 아직 방문하지 않은 곳이라면 방문
+                if(array[nextY][nextX] != 'x' && check[nextY][nextX] == false){
+                    if(dfs(nextY, nextX)){
+                        return true;
+                    }
+                }
             }
         }
+        return false; // 방문 못했을 경우 false
     }
 }
